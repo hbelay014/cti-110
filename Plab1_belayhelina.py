02b5a44e9bdffa298bc8b22dc1fe249b62690955
hbelay014 <belayhelina11@gmail.com>
2024-09-25 18:22:11 -0400

    Add files via upload

---
+++ Plab1_belayhelina.py
@@ -0,0 +1,13 @@
+# Your name: [ Helina Belay]
+# Date: 09-25-2024
+# Assignment Name: Lab 1
+# Discription: This program takes the user's first and last name as input
+# and displays a welcome message that includes their full name.
+
+# Prompt the user for their first name
+first_name= input( "Enter your first name:")
+# Prompt the youser for their last name
+last_name= input (" Enter your last name:")
+# Display the welcome message
+print (f"Hello, {first_name} {last_name}! Welcome to CTI 110")
+
